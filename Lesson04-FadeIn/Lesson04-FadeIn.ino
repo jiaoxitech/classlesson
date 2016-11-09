401f6a9481de4bdee35e42d10e6e6656510b875c
Will Fong <w@r10e.xyz>
2016-11-09 11:12:34 +0800

    Lesson 04

---
+++ Lesson04-FadeIn/Lesson04-FadeIn.ino
@@ -0,0 +1,26 @@
+const int GREEN_LED = 9;
+const int YELLOW_LED = 6;
+const int RED_LED = 5;
+
+int x = 0;
+
+void setup() {
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+
+}
+
+void loop() {
+  analogWrite(GREEN_LED, x);
+  analogWrite(YELLOW_LED, x);
+  analogWrite(RED_LED, x);
+
+  if ( x == 200 ) {
+    x = 0;
+  } else {
+    x = x + 1;
+  }
+  
+  delay(10);
+}
