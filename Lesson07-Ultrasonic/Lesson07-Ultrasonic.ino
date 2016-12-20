9ff7daae7be45e1507718b56818c832e1a4be05f
Will Fong <w@r10e.xyz>
2016-12-21 00:27:08 +0800

    Switched output to RGB LED

--- Lesson07-Ultrasonic/Lesson07-Ultrasonic.ino
+++ Lesson07-Ultrasonic/Lesson07-Ultrasonic.ino
@@ -1,20 +1,33 @@
-
-int vccPin = 4;
-int trigPin = 5;
-int echoPin = 6;
-int gndPin = 7;
+int vccPin = 14;
+int trigPin = 15;
+int echoPin = 16;
+int gndPin = 17;
+int ledVcc = 8;
+int redLed = 9;
+int blueLed = 10;
+int greenLed = 11;
 long duration, cm;
  
-void setup() {
-  Serial.begin (9600);
+void setup() 
+{
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(vccPin, OUTPUT);
   pinMode(gndPin, OUTPUT);
-
+ 
   digitalWrite(vccPin, HIGH);
   digitalWrite(trigPin, LOW);
   digitalWrite(gndPin, LOW);
+
+  pinMode(ledVcc, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+
+  digitalWrite(ledVcc, HIGH);
+  digitalWrite(redLed, HIGH);
+  digitalWrite(blueLed, HIGH);
+  digitalWrite(greenLed, HIGH);  
 }
  
 void loop()
@@ -26,8 +39,41 @@ void loop()
   duration = pulseIn(echoPin, HIGH);
   cm = duration / 58.0;
 
-  Serial.print(cm);
-  Serial.println(" cm");
-  
+  if ( cm > 100 ) 
+  {
+    colorGreen();
+  } 
+  else if ( cm > 50 )
+  {
+    colorYellow();
+  } 
+  else 
+  {
+    colorRed();
+  }
+
   delay(200);
 }
+
+void colorRed()
+{
+  analogWrite(redLed, 200);
+  analogWrite(blueLed, 255);
+  analogWrite(greenLed, 255);
+}
+
+void colorYellow()
+{
+  analogWrite(redLed, 200);
+  analogWrite(blueLed, 255);
+  analogWrite(greenLed, 200);
+}
+
+void colorGreen()
+{
+  analogWrite(redLed, 255);
+  analogWrite(blueLed, 255);
+  analogWrite(greenLed, 200);
+}
+
+
